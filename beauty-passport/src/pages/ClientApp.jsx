240357837984f4808ba6a67a638de881c7693b5b
kamonanban3ban-spec <kamonanban3ban@gmail.com>
2026-04-10 14:34:25 +0900

    Update ClientApp.jsx

--- beauty-passport/src/pages/ClientApp.jsx
+++ beauty-passport/src/pages/ClientApp.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'　
 import { INDUSTRIES } from '../industries'
 import { getClientByQrId, subscribeRecords, addClientPhotos } from '../firebase/db'
 
