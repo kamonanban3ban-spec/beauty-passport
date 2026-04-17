50ab745f55f4163a1d5cf9cd1b88a64c9b01a4b8
鴨川圭 <kamogawakei@kamogawakeinoMacBook-Air.local>
2026-04-17 23:51:30 +0900

    add salon URL generator

--- beauty-passport/src/main.jsx
+++ beauty-passport/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import StaffApp from './pages/StaffApp'
 import ClientApp from './pages/ClientApp'
-
+import GenerateSalonURL from './pages/GenerateSalonURL'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
         {/* お客様用URL: /client?salon=hair&qr=hp_xxx */}
         <Route path="/client" element={<ClientApp />} />
-
+<Route path="/admin" element={<GenerateSalonURL />} />
         {/* デフォルト → スタッフ画面 */}
         <Route path="*" element={<Navigate to="/staff" replace />} />
       </Routes>
